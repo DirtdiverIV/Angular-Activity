17977a50ba68904d61b5b59bcdbcb0facd91a36e
DirtdiverIV <davidgomez.seg@gmail.com>
2023-06-26 23:34:43 +0200

    Repaired home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,13 +27,19 @@ export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
   filteredLocationList: HousingLocation[] = [];
-
-  
-
   constructor() {
     this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
       this.housingLocationList = housingLocationList;
       this.filteredLocationList = housingLocationList;
     });
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
